e5b41b7cf9f3f955bbff7e12bc253b791c5eb18f
Vittorio Dinovi <vito.dinovi@gmail.com>
2016-09-28 18:27:31 -0700

    removed swap files

--- cgi-bin/clear.php
+++ cgi-bin/clear.php
@@ -1,8 +1,12 @@
 <?php
-    $realPass = "Vd12345!";
+    # store this in  /etc/pass
+    $passfile = fopen("../../etc/pass", "r") or die("TODO:send http error");
+    
+    #$user = key(
+
     $suppliedPass = key($_POST);
     if (!strcmp($realPass, $suppliedPass)) {
-        $conn = new mysqli('simpledb.db', 'simple_server_access', $realPass, 'simple_server');
+        $conn = new mysqli('simpledb.db', $user, $pass, 'simple_server');
         if (!$conn->connect_error) {
             mysqli_query($conn, "DELETE FROM users;");
         } 
