39e37232408586799e80563bc299d08a88ff77d4
Vittorio Dinovi <vito.dinovi@gmail.com>
2016-09-30 22:45:05 -0700

    Added simple chat UI. Setup groundwork for message handling system

---
+++ cgi-bin/chat.php
@@ -0,0 +1,7 @@
+<?php
+    include 'util.php';
+    // Process http message here. Return 
+    // - Stick the message in a queue(where?) to be picked up by chat window poll.
+    // - http://www.ebrueggeman.com/blog/creating-a-message-queue-in-php
+    // - Learn about AJAX long polling?
+?>
