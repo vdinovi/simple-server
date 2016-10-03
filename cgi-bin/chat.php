cc206c1960224f543bc38648fa4b78c2705e6594
Vittorio Dinovi <vdinovi@calpoly.edu>
2016-10-03 15:47:10 -0700

    Update chat.php

--- cgi-bin/chat.php
+++ cgi-bin/chat.php
@@ -1,7 +1,6 @@
 <?php
     include 'util.php';
     // Process http message here. Return 
-    // - Stick the message in a queue(where?) to be picked up by chat window poll.
-    // - http://www.ebrueggeman.com/blog/creating-a-message-queue-in-php
-    // - Learn about AJAX long polling?
+    // - Stick the message in a queue (stored as a session var) to be picked up later
+    // - Will have to use either AJAX long polling or websockets to get realtime chat updates
 ?>
